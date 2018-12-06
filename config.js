2cc54dfdda85775546eb265265a4b0e4718c8d93
thisismrsanjay <37665041+thisismrsanjay@users.noreply.github.com>
2018-12-06 16:12:32 +0530

    Update config.js

--- config.js
+++ config.js
@@ -2,21 +2,21 @@ module.exports={
     mailer:{
         service:'Gmail',
         auth:{
-            user:'tutorsfactory@gmail.com',
-            pass:'Isuyash210@123',
+            user:'',
+            pass:'',
         }
     },
-    database : 'mongodb://sanjay:sanjay123@ds121183.mlab.com:21183/itutor',
+    database : 'mongodb://<username>:<dbpass>@ds121183.mlab.com:21183/itutor',
     secret:'glorious',
     facebook:{
-        clientID:'255732381795481',
-        clientSecret:'9483f0e90a568e7b46b5dea15d752b68',
+        clientID:'',
+        clientSecret:'',
         callbackURL:'http://localhost:3000/auth/facebook/call',
         profileFields:['id','displayName','email']
     },
     google:{
-        googleClientID:'676597316116-1u8paqloho548350i48o09up3bslbj2m.apps.googleusercontent.com',
-        googleClientSecret:'F1shVAxm4U4Lq5qhWr6nljLz',
+        googleClientID:'',
+        googleClientSecret:'',
         callbackURL:'/auth/google/call'
     }
     
